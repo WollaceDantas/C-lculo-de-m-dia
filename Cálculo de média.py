919afd849ee782350c2bcc2938ed68a8682bd2b6
Wollace Cavalcante Dantas <wollacecavalcante@outlook.com.br>
2022-10-14 17:54:51 -0300

    Cálculo de média

---
+++ Cálculo de média.py
@@ -0,0 +1,15 @@
+# Exercício 2
+
+nota1= float(input("Digite a primeira nota:"))
+nota2= float(input("Digite a segunda nota:"))
+
+media=(nota1+nota2)/2
+
+print("Média igual a",media)
+print(media)
+
+if media>=6:
+    print("Aprovado.")
+
+else:
+    print("Reprovado.")
